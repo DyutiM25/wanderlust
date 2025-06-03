da531b1aa07a333fc0b49db10888526ccc2880fa
Dyuti Mengji <mengjidyuti@gmail.com>
2025-06-03 04:15:14 -0700

    end of phase 1 - css, error handling, middleware

--- app.js
+++ app.js
@@ -1,37 +1,149 @@
 const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
-const Listing = require("../wanderlust/models/listing.js")
-app.get("/",(req, res)=>{
-    res.send("Hey! I am Root.")
+const Listing = require("../wanderlust/models/listing.js");
+const path = require("path");
+const methodOverride = require("method-override");
+const ejsMate = require("ejs-mate");
+const wrapAsync = require("./utils/wrapAsync.js");
+const ExpressError = require("./utils/ExpressError.js");
+const { listingSchema } = require("./schema.js");
+
+app.get("/", (req, res) => {
+  res.send("Hey! I am Root.");
 });
 
 const MONGO_URL = "mongodb://127.0.0.1:27017/wanderlust";
 
 main()
-    .then(() =>{
-        console.log("connected to DB");
-    })
-    .catch((err) => {
-        console.log(err);
-    })
+  .then(() => {
+    console.log("connected to DB");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
 async function main() {
   await mongoose.connect(MONGO_URL);
 }
 
-app.get("/testListing", async (req, res) => {
+app.set("view engine", "ejs");
+app.set("views", path.join(__dirname, "views"));
+app.use(express.urlencoded({ extended: true }));
+app.use(methodOverride("_method"));
+app.engine("ejs", ejsMate);
+app.use(express.static(path.join(__dirname, "public")));
+
+const validateListing = (req, res, next) => {
+  let { error } = listingSchema.validate(req.body);
+  if (error) {
+    let errMsg = error.details.map((el) => el.message).join(",");
+    throw new ExpressError(400, errMsg);
+  } else {
+    next();
+  }
+};
+
+app.get(
+  "/testListing",
+  wrapAsync(async (req, res) => {
     let sampleListing = new Listing({
-        title: "My New Villa",
-        description: "By the Beach",
-        price: 1200,
-        location: "Somewhere, Goa",
-        country: "India",
+      title: "My New Villa",
+      description: "By the Beach",
+      price: 1200,
+      location: "Somewhere, Goa",
+      country: "India",
     });
     await sampleListing.save();
     console.log("sample was saved");
     res.send("successful testing");
+  })
+);
+
+//Index Route
+app.get(
+  "/listings",
+  wrapAsync(async (req, res, next) => {
+    const allListings = await Listing.find({});
+    res.render("listings/index.ejs", { allListings });
+  })
+);
+
+//New Route
+app.get("/listings/new", (req, res, next) => {
+  res.render("listings/new.ejs");
 });
 
-app.listen(8080, ()=>{
-    console.log("Server is listening to Port 8080");
+//Show Route
+app.get(
+  "/listings/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+  })
+);
+
+//Create Route
+app.post(
+  "/listings",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let result = listingSchema.validate(req.body);
+    console.log(result);
+    if (result.error) {
+      throw new ExpressError(400, result.error);
+    }
+    const newListing = new Listing(req.body.listing);
+    await newListing.save();
+    res.redirect("/listings");
+  })
+);
+
+//Edit Route
+app.get(
+  "/listings/:id/edit",
+  wrapAsync(async (req, res,next) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/edit.ejs", { listing });
+  })
+);
+
+//Update Route
+app.put(
+  "/listings/:id",
+  validateListing,
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    await Listing.findByIdAndUpdate(id, { ...req.body.listing });
+    res.redirect(`/listings/${id}`);
+  })
+);
+
+//Delete Route
+app.delete(
+  "/listings/:id",
+  wrapAsync(async (req, res, next) => {
+    let { id } = req.params;
+    let deletedListing = await Listing.findByIdAndDelete(id);
+    console.log(deletedListing);
+    res.redirect("/listings");
+  })
+);
+
+app.all(/(.*)/, (req, res, next) => {
+  next(new ExpressError(404, "Page not Found!"));
 });
+
+app.use((err, req, res, next) => {
+  let { statusCode = 500, message = "Something went wrong!" } = err;
+  res.status(statusCode).render("error.ejs", { message });
+  // res.render("error.ejs", {err});
+});
+
+app.listen(8080, () => {
+  console.log("Server is listening to Port 8080");
+});
+
+// npm i ejs-mate
+// joi for schema validation
